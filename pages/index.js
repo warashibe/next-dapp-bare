bf1a24638821b9a4f8b87b51444c89b86bb86110
Tomoya Nagasawa <ocrybit@gmail.com>
2020-05-31 03:06:28 +0200

    render nd.config.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,47 @@
 import { useEffect, Fragment } from "react"
 import R from "ramdam"
-import binder from "lib/binder"
+import binder, { bind } from "lib/binder"
+import { Conf as _conf } from "nd-core"
+const [Conf] = bind([_conf])
+
+const Count = binder(
+  props => (
+    <div style={{ textAlign: "center" }}>
+      <div
+        style={{
+          display: "inline-block",
+          margin: "20px",
+          cursor: "pointer",
+          backgroundColor: "#222",
+          color: "white",
+          padding: "10px",
+          borderRadius: "3px"
+        }}
+        onClick={() => {
+          props.set((props.count || 0) + 1, "count")
+        }}
+      >
+        count: {props.count || 0}
+      </div>{" "}
+      click to add count
+    </div>
+  ),
+  ["count"],
+  ["set"]
+)
 
 export default binder(
   props => {
+    useEffect(() => {
+      props.test()
+    }, [])
     return (
-      <div style={{ textAlign: "center" }}>
-        <div
-          style={{
-            display: "inline-block",
-            margin: "20px",
-            cursor: "pointer",
-            backgroundColor: "#222",
-            color: "white",
-            padding: "10px",
-            borderRadius: "3px"
-          }}
-          onClick={() => {
-            props.set((props.count || 0) + 1, "count")
-          }}
-        >
-          count: {props.count || 0}
-        </div>{" "}
-        click to add count
-      </div>
+      <Fragment>
+        <Count />
+        <Conf />
+      </Fragment>
     )
   },
-  ["count"],
-  ["set"]
+  [],
+  ["test"]
 )
