48a00dbb85ce5bdadbaae3b78243496f7c4e1074
Tomoya Nagasawa <ocrybit@gmail.com>
2020-06-09 04:45:56 +0200

    pass React to bind

--- nd/bind.js
+++ nd/bind.js
@@ -1,5 +1,6 @@
 import { bind } from "@nextdapp/core"
 import { connect } from "react-redux"
 import conf from "nd/conf"
+import React from "react"
 
-export default bind(connect, conf)
+export default bind(connect, conf, React)
