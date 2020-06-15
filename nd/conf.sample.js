fd548390a8442a93fc57579acdaec4c4b540c343
Tomoya Nagasawa <ocrybit@gmail.com>
2020-06-15 02:41:36 +0200

    change cover image size

--- nd/conf.sample.js
+++ nd/conf.sample.js
@@ -11,7 +11,7 @@ const prod = {
     title: "Next Dapp | The Bridge between Web 2.0 and 3.0",
     description:
       "Next Dapp is a web framework to progressively connect web 2.0 with 3.0.",
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/1000/500",
     "theme-color": "#03414D"
   }
 }
